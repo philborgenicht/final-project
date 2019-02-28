c3d4d890c67cc8adbb9c50fdd32f699a676d1851
Phillip Borgenicht <philborgenicht@gmail.com>
2019-02-28 10:11:58 -0800

    firebase

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('./config/passport')
 
 var express = require('express');
 var path = require('path');
@@ -7,16 +8,38 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var athletesRouter= require('./routes/athletes');
+var customersRouter=require('./routes/customers');
+var teamsRouter=require('./routes/teams');
+var sportsRouter=require('./routes/sports');
+var favoriteTeamsRouter=require('./routes/favorite_teams')
+var favoriteAthletesRouter=require('./routes/favorite_athletes')
+var favoriteSportsRouter=require('./routes/favorite_sports')
+var rostersRouter=require('./routes/rosters');
+var passport = require('passport');
 
 var app = express();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/athletes', athletesRouter)
+app.use('/customers', customersRouter)
+app.use('/teams', teamsRouter)
+app.use('/sports', sportsRouter)
+app.use('/favorite_teams', favoriteTeamsRouter)
+app.use('/favorite_sports', favoriteSportsRouter)
+app.use('/favorite_athletes', favoriteAthletesRouter)
+app.use('/rosters', rostersRouter)
 module.exports = app;
